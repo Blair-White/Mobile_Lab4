679f882f97b5ae5b40e10a44d2e758abcb567738
Blair-White <blairjwhite@hotmail.com>
2020-10-14 03:28:55 -0400

    Finished follow along.

---
+++ GAME2014-F2020-Lab4a-main/Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float horizontalSpeed, horizontalBoundary, direction;
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Move()
+    {
+        transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+
+    }
+
+    private void _CheckBounds()
+    {
+        //check right bound
+        if(transform.position.x >= horizontalBoundary)
+        {
+            direction = -1.0f;
+        }
+        //check left bound
+        if (transform.position.x <= -horizontalBoundary)
+        {
+            direction = 1.0f;
+        }
+    }
+}
